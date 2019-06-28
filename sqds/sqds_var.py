f625cd20befe09c6fd0f416c50536aae8ec11de4
1976hjj <1071304055@qq.com>
2019-06-28 11:45:58 +0800

    Delete sqds_var.py

--- sqds/sqds_var.py
+++
@@ -1,9 +0,0 @@
-# from han_test import *
-#
-# sqds_sc = SparkSession.builder.master("local[5]") \
-#     .appName("SQDS") \
-#     .enableHiveSupport() \
-#     .getOrCreate()
-# sqds_sc.sparkContext.setLogLevel("WARN")
-# print(sqds_sc)
-# print(datetime.datetime.today())
